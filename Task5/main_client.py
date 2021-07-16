3ab265f1bcca030b6675d64af1d28a9702c229e2
devscheffer <devscheffer@gmail.com>
2021-07-15 22:20:28 -0300

    Entrega final

--- Task5/main_client.py
+++ Task5/main_client.py
@@ -1,9 +1,9 @@
+from Task5.src.client import cls_client
 from os import getcwd
 from sys import path
 
 cwd = getcwd()
 path.append(cwd)
-from Task5.src.client import cls_client
 
 
 # buffer = 1024
@@ -12,19 +12,19 @@ host = "localhost"
 port = 50007
 
 while True:
-	try:
-		client_user_name = input("Nome de usuario:\n ")
-		group = input("Grupo do usuario:\n ")
+    try:
+        client_user_name = input("Nome de usuario:\n ")
+        group = input("Grupo do usuario:\n ")
 
-		escritor = cls_client(
-			client_user_name=client_user_name,
-			group=group,
-			buffer=buffer,
-			host=host,
-			port=port,
-		)
-		break
-	except Exception as e:
-		print(f"{e}")
-		continue
+        escritor = cls_client(
+            client_user_name=client_user_name,
+            group=group,
+            buffer=buffer,
+            host=host,
+            port=port,
+        )
+        break
+    except Exception as e:
+        print(f"{e}")
+        continue
 escritor.mtd_client_start()
--- Task5/src/log.py
+++ Task5/src/log.py
@@ -1,11 +1,14 @@
 import datetime
 
-def fn_write_log(event_type:str,event_data:dict,file_name:str='geral',log_path:str="./Task5/data"):
-    path_output = f'{log_path}/log_{file_name}.txt'
-    log_text = {
-                "event_type": event_type,
-                "event_data": event_data
-            }
+
+def fn_write_log(
+    event_type: str,
+    event_data: dict,
+    file_name : str = "geral",
+    log_path  : str = "./Task5/log"
+    ) -> None:
+    path_output = f"{log_path}/log_{file_name}.txt"
+    log_text = {"event_type": event_type, "event_data": event_data}
     with open(path_output, "a") as file:
         current_time = datetime.datetime.now()
         file.write(f"{current_time},{log_text}\n")
